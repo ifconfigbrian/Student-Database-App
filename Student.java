9cbc9d7c05f5a481b4ee6227495aada053be6221
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 09:46:46 +0300

    added prompt method in student constructor

--- Student.java
+++ Student.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentId;
     private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
+    private static int id = 1000;
 
     //constructor:prompt user to enter students name and year
+    public Student(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Students' first name: ");
+        this.firstName = sc.nextLine();
+
+        System.out.print("Enter Students' last name: ");
+        this.lastName = sc.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 Senior\nEnter student class level: ");
+        this.gradeYear = sc.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+
+        id++;
+    }
     //generate an id
     //enroll in courses
     //view balance
