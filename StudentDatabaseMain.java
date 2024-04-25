72ae251db7e756881d1f6b164e1029ae6f7c1868
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 09:22:38 +0300

    created an empty main method

---
+++ StudentDatabaseMain.java
@@ -0,0 +1,7 @@
+public class StudentDatabaseMain {
+
+    public static void main(String[] args) {
+        //ask how many new users we want to add
+        //create number of new students
+    }
+}
