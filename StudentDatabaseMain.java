4d3d6f175262236b81741d0a29f634829d61ee11
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 10:01:52 +0300

    created a new student

--- StudentDatabaseMain.java
+++ StudentDatabaseMain.java
@@ -1,6 +1,7 @@
 public class StudentDatabaseMain {
 
     public static void main(String[] args) {
+        Student stu1 = new Student();
         //ask how many new users we want to add
         //create number of new students
     }
