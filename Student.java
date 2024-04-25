8840f81567818ca862bbca59a6d904c89e4d3901
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 10:56:32 +0300

    created an enroll method

--- Student.java
+++ Student.java
@@ -5,25 +5,25 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentId;
-    private String courses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private int costOfCourse = 600;
     private static int id = 1000;
 
     //constructor:prompt user to enter students name and year
     public Student(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Students' first name: ");
+        System.out.print("Enter Students first name: ");
         this.firstName = sc.nextLine();
 
-        System.out.print("Enter Students' last name: ");
+        System.out.print("Enter Students last name: ");
         this.lastName = sc.nextLine();
 
-        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 Senior\nEnter student class level: ");
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
         this.gradeYear = sc.nextInt();
-        
+
         setStudentId();
-        System.out.println(firstName + " " + lastName + " " + gradeYear); 
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId); 
     }
     //generate an id
     private void setStudentId(){
@@ -33,6 +33,24 @@ public class Student {
     }
    
     //enroll in courses
+     public void enroll(){
+        //enter a loop,user hits Q to quit
+        do{
+        System.out.println("Enter course to enroll(Q to quit): ");
+        Scanner sc = new Scanner(System.in);
+        String course = sc.nextLine();
+        if (!course.equals("Q")) {
+            courses = courses + "\n" + course;
+            tuitionBalance = tuitionBalance + costOfCourse;
+            
+        }else{
+            break;
+        }
+    }while(1 != 0);
+
+        System.out.println("ENROLLED IN: " + courses);
+        System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
     //view balance
     //pay tuition
     //show status
