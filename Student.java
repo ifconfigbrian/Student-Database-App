4adb7dbab92ea6967d4134bf276b03be3bc7fca9
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 09:23:14 +0300

    created an outline and methods

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    //constructor:prompt user to enter students name and year
+    //generate an id
+    //enroll in courses
+    //view balance
+    //pay tuition
+    //show status
+
+}
