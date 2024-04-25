afa8f39cdc5cc10497340c5b89f35cab0e6f3431
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 19:50:42 +0300

    called the tostring method in a print statement

--- StudentDatabaseMain.java
+++ StudentDatabaseMain.java
@@ -4,6 +4,7 @@ public class StudentDatabaseMain {
         Student stu1 = new Student();
         stu1.enroll();
         stu1.payTuition();
+        System.out.println(stu1.toString());
         //ask how many new users we want to add
         //create number of new students
     }
