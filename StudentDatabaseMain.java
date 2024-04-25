809982f5c0ed5013d803a4b09ce5af63b1fbf561
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 19:17:37 +0300

    called the paytution method

--- StudentDatabaseMain.java
+++ StudentDatabaseMain.java
@@ -3,6 +3,7 @@ public class StudentDatabaseMain {
     public static void main(String[] args) {
         Student stu1 = new Student();
         stu1.enroll();
+        stu1.payTuition();
         //ask how many new users we want to add
         //create number of new students
     }
