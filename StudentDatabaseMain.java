85961c60d63c51d164c3b07aa486f31f130ff2b7
stewartbrian23 <njugunabriian@gmail.com>
2024-04-25 20:13:37 +0300

    created an array of obejcts

--- StudentDatabaseMain.java
+++ StudentDatabaseMain.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class StudentDatabaseMain {
 
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
         //ask how many new users we want to add
+        System.out.println("How many students do you want to enroll? : ");
+        Scanner sc = new Scanner(System.in);
+        int numOfStudents = sc.nextInt();
+        Student[]students = new Student[numOfStudents];
         //create number of new students
+        for(int n = 0; n < numOfStudents; n++){
+            students[n]= new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].toString());
+
+        }
     }
 }
